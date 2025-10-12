9d7156a91c90b1f82d39d486d0dd7b3fa460bdd8
David Belais <david@belais.me>
2025-10-12 15:27:37 -0700

    *

--- hatch_plugins.py
+++ hatch_plugins.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import re
+from contextlib import suppress
 from pathlib import Path
 
 from hatch.env.collectors.plugin.interface import EnvironmentCollectorInterface
@@ -20,16 +21,17 @@ class DotEnvCollectorInterface(EnvironmentCollectorInterface):
         self, config: dict[str, dict]
     ) -> dict[str, dict]:
         env: dict[str, str] = {}
-        with ENV.open(encoding="utf-8") as env_io:
-            line: str
-            for line in env_io:
-                match: re.Match | None = PATTERN.match(line)
-                if match is not None:
-                    env[match.group(1)] = match.group(2)
-        environment: dict[str, dict[str, str]]
-        for environment in config.values():
-            if "env-vars" not in environment:
-                environment["env-vars"] = env
-            else:
-                environment["env-vars"].update(env)
+        with suppress(FileNotFoundError):
+            with ENV.open(encoding="utf-8") as env_io:
+                line: str
+                for line in env_io:
+                    match: re.Match | None = PATTERN.match(line)
+                    if match is not None:
+                        env[match.group(1)] = match.group(2)
+            environment: dict[str, dict[str, str]]
+            for environment in config.values():
+                if "env-vars" not in environment:
+                    environment["env-vars"] = env
+                else:
+                    environment["env-vars"].update(env)
         return config
