8cbd3f29fc882bbfcbe83ebc170092ecd3cf476d
David Belais <david@belais.me>
2025-10-12 16:11:22 -0700

    *

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,19 +1,17 @@
 import os
+
 from decorative_secrets.environment import apply_environment_arguments
 
 
 def test_apply_environment_arguments() -> None:
-    apply_environment_arguments(
-        token="OP_SERVICE_ACCOUNT_TOKEN"
-    )
+    apply_environment_arguments(token="OP_SERVICE_ACCOUNT_TOKEN")
+
     def get_token(
         token: str | None = None,
         token_environment_variable: str | None = None,
     ) -> str | None:
         return token
-    
+
     assert get_token(
         token_environment_variable="OP_SERVICE_ACCOUNT_TOKEN"
-    ) == get_token(
-        token=os.getenv("OP_SERVICE_ACCOUNT_TOKEN")
-    )
+    ) == get_token(token=os.getenv("OP_SERVICE_ACCOUNT_TOKEN"))
